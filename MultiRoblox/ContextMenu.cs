3fc5195a43e3cef55ec260924baa2604724b0466
Daniel Lun <daniel@dlun.tk>
2025-01-20 21:13:32 -0800

    Optimize code readability

---
+++ MultiRoblox/ContextMenu.cs
@@ -0,0 +1,52 @@
+﻿using System.Runtime.InteropServices;
+
+namespace MultiRoblox
+{
+    internal class ContextMenu
+    {
+        [DllImport("dwmapi.dll")]
+        private static extern int DwmSetWindowAttribute(IntPtr hwnd, int dwAttribute, ref int pvAttribute, int cbAttribute);
+
+        NotifyIcon? NotifyIcon;
+        About? About;
+
+        public ContextMenu()
+        {
+            ContextMenuStrip ContextMenuStrip = new ContextMenuStrip();
+            ContextMenuStrip.Items.Add(new ToolStripMenuItem("About", null, ShowAbout));
+            ContextMenuStrip.Items.Add(new ToolStripMenuItem("Exit", null, Exit));
+
+            NotifyIcon = new NotifyIcon();
+            NotifyIcon.Icon = Properties.Resources.AppIcon;
+
+            NotifyIcon.ContextMenuStrip = ContextMenuStrip;
+
+            NotifyIcon.Text = "MultiRoblox";
+            NotifyIcon.Visible = true;
+        }
+
+        void Exit(object? sender, EventArgs e)
+        {
+            NotifyIcon?.Dispose();
+            Application.Exit();
+        }
+
+        void ShowAbout(object? sender, EventArgs e)
+        {
+            if (About == null || About.IsDisposed)
+            {
+                About = new About();
+
+                int useDarkMode = 1;
+                DwmSetWindowAttribute(About.Handle, 20, ref useDarkMode, sizeof(int));
+
+                About.Show();
+            }
+            else
+            {
+                About.BringToFront();
+                About.Activate();
+            }
+        }
+    }
+}
--- MultiRoblox/MultiRoblox.cs
+++ MultiRoblox/MultiRoblox.cs
@@ -1,41 +1,18 @@
-using System.Runtime.InteropServices;
-
 namespace MultiRoblox
 {
     internal static class MultiRoblox
     {
-        [DllImport("dwmapi.dll")]
-        private static extern int DwmSetWindowAttribute(IntPtr hwnd, int dwAttribute, ref int pvAttribute, int cbAttribute);
+        static ContextMenu? ContextMenu;
 
-        static NotifyIcon? NotifyIcon;
-        static About? About;
         static void Main()
         {
-            if (Mutex.TryOpenExisting("MultiRoblox_singletonMutex", out var _))
+            if (CheckIfRunning())
             {
-                MessageBox.Show("MultiRoblox already running.", "MultiRoblox", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 Application.Exit();
                 return;
             }
 
-            if (Mutex.TryOpenExisting("ROBLOX_singletonMutex", out var _))
-            {
-                MessageBox.Show("Roblox already running. Please run MultiRoblox before starting Roblox.", "MultiRoblox", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                Application.Exit();
-                return;
-            }
-
-            ContextMenuStrip ContextMenuStrip = new ContextMenuStrip();
-            ContextMenuStrip.Items.Add(new ToolStripMenuItem("About", null, ShowAbout));
-            ContextMenuStrip.Items.Add(new ToolStripMenuItem("Exit", null, Exit));
-
-            NotifyIcon = new NotifyIcon();
-            NotifyIcon.Icon = Properties.Resources.AppIcon;
-
-            NotifyIcon.ContextMenuStrip = ContextMenuStrip;
-
-            NotifyIcon.Text = "MultiRoblox";
-            NotifyIcon.Visible = true;
+            new ContextMenu();
 
             new Mutex(true, "MultiRoblox_singletonMutex");
             new Mutex(true, "ROBLOX_singletonMutex");
@@ -43,28 +20,21 @@ namespace MultiRoblox
             Application.Run();
         }
 
-        static void Exit(object? sender, EventArgs e)
+        static bool CheckIfRunning()
         {
-            NotifyIcon?.Dispose();
-            Application.Exit();
-        }
-
-        static void ShowAbout(object? sender, EventArgs e)
-        {
-            if (About == null || About.IsDisposed)
+            if (Mutex.TryOpenExisting("MultiRoblox_singletonMutex", out var _))
             {
-                About = new About();
+                MessageBox.Show("MultiRoblox is already running.", "MultiRoblox", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return true;
+            }
 
-                int useDarkMode = 1;
-                DwmSetWindowAttribute(About.Handle, 20, ref useDarkMode, sizeof(int));
-                
-                About.Show();
-            }
-            else
+            if (Mutex.TryOpenExisting("ROBLOX_singletonMutex", out var _))
             {
-                About.BringToFront();
-                About.Activate();
+                MessageBox.Show("Roblox is already running. Please run MultiRoblox before starting Roblox.", "MultiRoblox", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return true;
             }
+
+            return false;
         }
     }
 }
